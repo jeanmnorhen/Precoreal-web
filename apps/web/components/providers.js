846aa3d13b8cd10d8ecd8432d3ee0e4da06d4bb2
jeanmnorhen <jeanmdasleite@gmail.com>
2026-06-14 22:23:28 -0300

    v

--- apps/web/components/providers.js
+++
@@ -1,8 +0,0 @@
-'use client';
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Providers = Providers;
-const auth_context_1 = require("@/lib/auth-context");
-function Providers({ children }) {
-    return <auth_context_1.AuthProvider>{children}</auth_context_1.AuthProvider>;
-}
